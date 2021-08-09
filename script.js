c9fbb9b5effac9c2217b2a54ba7fe5eeac1c365e
Henry Peavler <treypeavler@icloud.com>
2021-08-09 15:04:41 -0500

    added classes, timebblock

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $("#currentDay").text(moment().format("dddd,MMMM Do"))
 $(".saveBtn").on("click",function(){
     var time = $(this).parent().attr("id")
-    var input = $(this).siblings(".descriptions").val()
+    var input = $(this).siblings(".description").val()
     localStorage.setItem(time,input)
 })
 $("#hour-9 .description").val(localStorage.getItem("hour-9"))
@@ -13,3 +13,20 @@ $("#hour-2 .description").val(localStorage.getItem("hour-2"))
 $("#hour-3 .description").val(localStorage.getItem("hour-3"))
 $("#hour-4 .description").val(localStorage.getItem("hour-4"))
 $("#hour-5 .description").val(localStorage.getItem("hour-5"))
+function HourCheck (){
+    var currentTime = moment().hours()
+    $(".time-block").each(function(){
+        var compairTime = parseInt($(this).attr("id").split("-")[1])
+        if(compairTime === currentTime){
+            $(this).addClass("present")
+        }else if(compairTime<currentTime){
+            $(this).removeClass("present")
+            $(this).addClass("past")
+        }else {
+            $(this).removeClass("past")
+            $(this).removeClass("present")
+            $(this).addClass("future")
+        }
+    })
+}
+HourCheck()
