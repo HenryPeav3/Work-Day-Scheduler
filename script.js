c09f429bd0a8c8669fea38b9af64a5f503f2c69e
Henry Peavler <treypeavler@icloud.com>
2021-08-15 19:54:50 -0500

    finishing toucn

--- script.js
+++ script.js
@@ -8,20 +8,23 @@ $("#hour-9 .description").val(localStorage.getItem("hour-9"))
 $("#hour-10 .description").val(localStorage.getItem("hour-10"))
 $("#hour-11 .description").val(localStorage.getItem("hour-11"))
 $("#hour-12 .description").val(localStorage.getItem("hour-12"))
-$("#hour-1 .description").val(localStorage.getItem("hour-1"))
-$("#hour-2 .description").val(localStorage.getItem("hour-2"))
-$("#hour-3 .description").val(localStorage.getItem("hour-3"))
-$("#hour-4 .description").val(localStorage.getItem("hour-4"))
-$("#hour-5 .description").val(localStorage.getItem("hour-5"))
+$("#hour-13 .description").val(localStorage.getItem("hour-13"))
+$("#hour-14 .description").val(localStorage.getItem("hour-14"))
+$("#hour-15 .description").val(localStorage.getItem("hour-15"))
+$("#hour-16 .description").val(localStorage.getItem("hour-16"))
+$("#hour-17 .description").val(localStorage.getItem("hour-17"))
 function HourCheck (){
-    var currentTime = moment().hours()
+    var currentTime = moment().hour()
     $(".time-block").each(function(){
-        var compairTime = parseInt($(this).attr("id").split("-")[1])
-        if(compairTime === currentTime){
-            $(this).addClass("present")
-        }else if(compairTime<currentTime){
-            $(this).removeClass("present")
+        var compareTime = parseInt($(this).attr("id").split("-")[1])
+        console.log(currentTime)
+        if(compareTime<currentTime){
+            console.log("past")
             $(this).addClass("past")
+        }else if(compareTime===currentTime){
+            console.log("present")
+            $(this).removeClass("past")
+            $(this).addClass("present")
         }else {
             $(this).removeClass("past")
             $(this).removeClass("present")
@@ -30,3 +33,4 @@ function HourCheck (){
     })
 }
 HourCheck()
+var check = setInterval(HourCheck,10000)
